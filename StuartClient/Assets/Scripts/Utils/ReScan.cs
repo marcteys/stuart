2f2e2bd718a1ca5f66d6121c43231a190992b281
Marc Teyssier <marc.teys@gmail.com>
2014-04-04 13:03:57 +0200

    adding helper tool scene

---
+++ StuartClient/Assets/Scripts/Utils/ReScan.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReScan : MonoBehaviour {
+
+	public UDPSend udpScript;
+
+
+	// Use this for initialization
+	public void Rescan () {
+		udpScript.sendString("rescan");
+	}
+	
+
+}
